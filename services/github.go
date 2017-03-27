42a6f54ad3e51ac4c30cdc74c2194c424c67700a
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-03-27 02:53:31 +0200

    Simplify github implementation

--- services/github.go
+++ services/github.go
@@ -12,27 +12,30 @@ type Github struct{}
 
 // Check implements the NameChecker interface
 func (g *Github) Check(name string) (bool, error) {
-	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s", name))
+	result, err := g.check("users", name)
+
+	if !result {
+		return result, err
+	}
+
+	result, err = g.check("orgs", name)
+
+	return result, err
+}
+
+func (g *Github) check(entity, name string) (bool, error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.github.com/%s/%s", entity, name))
 	if err != nil {
 		// TODO: wrap error
 		return false, errors.New("Cannot determine name availability")
 	}
 	resp.Body.Close()
 
-	if resp.StatusCode != http.StatusNotFound {
+	if resp.StatusCode == http.StatusNotFound {
+		return true, nil
+	} else if resp.StatusCode == http.StatusOK {
 		return false, nil
 	}
 
-	resp, err = http.Get(fmt.Sprintf("https://api.github.com/orgs/%s", name))
-	if err != nil {
-		// TODO: wrap error
-		return false, errors.New("Cannot determine name availability")
-	}
-	resp.Body.Close()
-
-	if resp.StatusCode != http.StatusNotFound {
-		return false, nil
-	}
-
-	return true, nil
+	return false, errors.New("Cannot determine name availability")
 }
